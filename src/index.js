bcbe270b92b8c8a4e43bf3acacd64f893125c6a6
askbk <askbk@protonmail.com>
2019-01-30 12:56:16 +0100

    Fikset litt basic routing

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ const http = require("http").Server(app);
 const appdir = __dirname + "/app";
 const port = 3000;
 
+app.use(/^\/api/, (req, res, next) => {
+    console.log("api request");
+    res.send("hello");
+    next();
+});
+
 app.use("/", express.static(appdir));
 
 http.listen(port, () => {
