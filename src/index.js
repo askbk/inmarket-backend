93a14bd0fcca6b3c9f55a052c92c96c124cca383
askbk <askbk@protonmail.com>
2019-01-30 11:54:47 +0100

    Fikset litt mer basic ting

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const app = require("express")();
+const express = require("express");
 const http = require("http").Server(app);
+const appdir = __dirname + "/app";
+app.use(express.static(appdir));
 
 app.get("/", (req, res) => {
-    res.sendFile("/srv/http/inmarket-app/index.html");
+    res.sendFile(appdir + "/index.html");
 });
 
 http.listen(3000, () => {
