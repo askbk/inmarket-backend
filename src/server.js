2c8dafbf0bb0fbf8a71b9c450ed657a2ca167e9d
askbk <askbk@protonmail.com>
2019-02-05 23:02:38 +0100

    La til et par routes for å teste

--- src/server.js
+++ src/server.js
@@ -1,17 +1,30 @@
 const express = require("express");
 const app = express();
 const http = require("http").Server(app);
+const fs = require("fs");   //  For å teste API
 
 const appdir = __dirname + "/app";
+const test_path = __dirname + "/testdata";
 const port = 3000;
 
-app.use(/^\/api/, (req, res, next) => {
-    console.log("api request");
-    res.send("hello");
-    next();
+//  Let app take care of everything except API
+app.get(/^(?!\/api\/)/, express.static(appdir));
+
+app.use("/api/test", (req, res, next) => {
+    res.status(200).send('Hello world!');
 });
 
-app.use("/", express.static(appdir));
+app.post("/api/login", (req, res, next) => {
+    res.json(req.params);
+});
+
+app.get("/api/getUser/:id", (req, res, next) => {
+    fs.readFile(test_path + "/user.json", 'utf8', (err, data) => {
+        const users = JSON.parse( data );
+        const user = users[req.params.id]
+        res.json(user);
+    });
+});
 
 http.listen(port, () => {
     console.log("listening on port " + port);
