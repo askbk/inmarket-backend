9c047487ff92b8ab1bc727c9042ab0ff748d92a8
askbk <askbk@protonmail.com>
2019-02-06 20:31:20 +0100

    API endpoints

--- src/server.js
+++ src/server.js
@@ -2,20 +2,25 @@ const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 const fs = require("fs");   //  For å teste API
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
 
 const appdir = __dirname + "/app";
 const test_path = __dirname + "/testdata";
 const port = 3000;
 
+app.use(bodyParser.json());
+
 //  Let app take care of everything except API
 app.get(/^(?!\/api\/)/, express.static(appdir));
+
 
 app.use("/api/test", (req, res, next) => {
     res.status(200).send('Hello world!');
 });
 
 app.post("/api/login", (req, res, next) => {
-    res.json(req.params);
+    res.send(JSON.stringify(req.body));
 });
 
 app.get("/api/getUser/:id", (req, res, next) => {
@@ -26,6 +31,11 @@ app.get("/api/getUser/:id", (req, res, next) => {
     });
 });
 
+app.post("/api/register", (req, res, next) => {
+    const user = req.params;
+    res.json(user);
+});
+
 http.listen(port, () => {
-    console.log("listening on port " + port);
+    console.log("listening on port " + port + " at " + new Date().toTimeString());
 });
