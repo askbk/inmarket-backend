5870a8ff377ec3d8d7255ee416655516d0a7fb7a
askbk <askbk@protonmail.com>
2019-05-05 10:24:44 +0200

    Add model for interests

---
+++ src/models/interest/jobseekerInterest.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, Sq) => {
+    const Model = Sq.Model;
+    class JobseekerInterest extends Model {}
+    JobseekerInterest.init({
+        isActive: {
+            type: Sq.BOOLEAN,
+            allowNull: false
+        }
+    }, {
+        sequelize,
+        modelName: "jobseekerInterest"
+    });
+
+    return JobseekerInterest;
+}
