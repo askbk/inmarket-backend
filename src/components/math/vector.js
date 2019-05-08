45c8f4bf13a0b49d8f1f151a57a2a6e26ce854c8
askbk <askbk@protonmail.com>
2019-05-08 08:27:31 +0200

    Fix math errors and wrong usage of reduce()

--- src/components/math/vector.js
+++ src/components/math/vector.js
@@ -3,26 +3,33 @@ const magnitude = vector => {
     return Math.sqrt(vector
     .filter(e => {return !!e})
     .reduce((acc, cur) => {
-        acc += cur * cur;
-    }));
+        return acc + cur * cur;
+    }, 0));
 }
 
 // Calculates the dot product of two vectors
 const dotProduct = (vectorA, vectorB) => {
     return vectorA.reduce((acc, cur, idx) => {
         if (vectorB[idx]) {
-            return cur * vectorB[idx];
+            return acc + cur * vectorB[idx];
         }
 
         return 0;
-    });
+    }, 0);
 }
 
 // Return cosine of the angle between two vectors
-async cosine(vectorA, vectorB) => {
+const cosine = async (vectorA, vectorB) => {
+    console.log(vectorA);
+    console.log(vectorB);
     const magnitudeA = magnitude(vectorA);
     const magnitudeB = magnitude(vectorB);
+    const dot = dotProduct(vectorA, vectorB);
 
+    // console.log(`magnitudes: ${magnitudeA}, ${magnitudeB}`);
+    // console.log(`dot: ${dot}`)
+    //
+    // console.log(magnitudeA * magnitudeB);
     return dotProduct(vectorA, vectorB) / (magnitudeA * magnitudeB);
 }
 
