c0b7d132f4b74db6610f3aae8bc51ce7abb2123e
askbk <askbk@protonmail.com>
2019-05-11 17:15:46 +0200

    Add safety measure for ill-defined results of cosine calculation

--- src/components/math/vector.js
+++ src/components/math/vector.js
@@ -1,11 +1,15 @@
 // Calculates magnitude of the supplied vector
 const magnitude = vector => {
-    return Math.sqrt(vector
-    .filter(e => {return !!e})
-    .reduce((acc, cur) => {
-        return acc + cur * cur;
-    }, 0));
-}
+    return Math.sqrt(
+        vector
+            .filter(e => {
+                return !!e;
+            })
+            .reduce((acc, cur) => {
+                return acc + cur * cur;
+            }, 0)
+    );
+};
 
 // Calculates the dot product of two vectors
 const dotProduct = (vectorA, vectorB) => {
@@ -16,7 +20,7 @@ const dotProduct = (vectorA, vectorB) => {
 
         return 0;
     }, 0);
-}
+};
 
 // Return cosine of the angle between two vectors
 const cosine = (vectorA, vectorB) => {
@@ -24,7 +28,13 @@ const cosine = (vectorA, vectorB) => {
     const magnitudeB = magnitude(vectorB);
     const dot = dotProduct(vectorA, vectorB);
 
-    return dot / (magnitudeA * magnitudeB);
-}
+    const cos = dot / (magnitudeA * magnitudeB);
+
+    if (cos) {
+        return cos;
+    }
+
+    return 0;
+};
 
 module.exports = {magnitude, dotProduct, cosine};
