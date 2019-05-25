225bf89151e559af5b36829a78c2a6d58d4a88cc
askbk <askbk@protonmail.com>
2019-05-25 19:21:00 +0200

    Remove old SQL files

--- db/sql/testdata.sql
+++
@@ -1,5 +0,0 @@
-USE inmarket_db;
-
-INSERT INTO user (name, email, phone, kommuneNr, adminLevel, password,
-    createTime, userType, profilePicture)
-    VALUES ("ask", "ask@ask.no", "12345678", "1567", 0, "passord123", NOW(), 0, "img/stock-profile.jpg");
